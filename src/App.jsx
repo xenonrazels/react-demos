5a7a8529819988221cc46f05505a1b8c3083262c
xenonrazels <xenonrazels@gmail.com>
2025-04-19 18:15:57 +0545

    initalSetup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Greet from "./componets/greet";
+
+const App = ()=>{
+  return (
+    <div>
+      Hello World
+      <div>
+        
+      </div>
+      <Greet></Greet>
+    </div>
+  )
+  
+}
+export default App;
---
+++ src/componets/greet.jsx
@@ -0,0 +1,8 @@
+const Greet =()=>{
+    return (
+        <>
+        Hello from greet
+        </>
+    )
+}
+export default Greet;
